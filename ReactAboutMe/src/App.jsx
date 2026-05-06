5126607fbc416282021c7cfbd0616ef076476b14
tQ111 <tondisx@gmail.com>
2026-05-06 20:08:17 +0000

    w

--- ReactAboutMe/src/App.jsx
+++ ReactAboutMe/src/App.jsx
@@ -8,21 +8,40 @@ import Resume from "./pages/resume.jsx";
 function App() {
   return (
     <>
-      <nav className="navBar">
-        <Link to="/about" className="navItem">About</Link>
-        <Link to="/interests" className="navItem">Interests</Link>
-        <Link to="/projects" className="navItem">Projects</Link>
-        <Link to="/resume" className="navItem">Resume</Link>
+      <nav className = "navBar">
+        <Link to = "/about" className = "navItem">About</Link>
+        <Link to = "/interests" className = "navItem">Interests</Link>
+        <Link to = "/projects" className = "navItem">Projects</Link>
+        <Link to = "/resume" className = "navItem">Resume</Link>
       </nav>
 
       <Routes>
-        <Route path="/" element={<Navigate to="/about" />} />
+        <Route path = "/" element = {<Navigate to = "/about" />} />
 
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/interests" element={<Interests />} />
-        <Route path="/resume" element={<Resume />} />
+        <Route path = "/about" element = {<About />} />
+        <Route path = "/projects" element  = {<Projects />} />
+        <Route path = "/interests" element = {<Interests />} />
+        <Route path = "/resume" element = {<Resume />} />
       </Routes>
+
+      <footer>
+        <div className = "foot">
+          <h4>Socials</h4>
+          <p>
+            Instagram: @tondisx
+            <br></br>
+            Discord: .sibun
+          </p>
+        </div>
+        <div id = "vertRule"></div>
+        <div className = "foot">
+          test
+        </div>
+        <div id = "vertRule"></div>
+        <div className = "foot">
+          test
+        </div>
+      </footer>
     </>
   );
 }
